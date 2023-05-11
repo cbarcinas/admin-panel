0d765d08edd33bf0733ce48b3388741f49d54434
Craig Barcinas <cbarcinas@live.com>
2023-05-10 22:10:26 -0700

    added user router and product routes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,20 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
+          {/* Default */}
           <Route path="/">
             <Route index element={<Home />}></Route>
             <Route path="login" element={<Login />}></Route>
+            {/* users */}
             <Route path="users">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+            {/* products */}
+            <Route path="products">
+              <Route index element={<List />}></Route>
+              <Route path=":productId" element={<Single />}></Route>
               <Route path="new" element={<New />}></Route>
             </Route>
           </Route>
