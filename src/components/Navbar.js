ecb07b34385a9206ffa4d8dfeeeb6577e25b1729
Craig Barcinas <cbarcinas@live.com>
2023-05-15 17:52:32 -0700

    styles udpated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBullet
 
 const Navbar = () => {
   return (
-    <div className="text-md flex h-[50px] items-center text-zinc-600">
-      <div className="flex w-full items-center justify-between p-5">
+    <div className="text-md flex h-[50px] items-center border-b-[1.5px] text-zinc-600">
+      <div className="flex w-full items-center justify-between p-4">
         {/* Search Input */}
         <div className="flex items-center rounded-sm border-[1px] border-zinc-400 ">
           <input
@@ -21,24 +21,37 @@ const Navbar = () => {
         </div>
         {/* Secondary Nav Options */}
         <div className="flex items-center">
-          <div className="item">
+          <div className="mr-4 flex items-center">
             <LanguageOutlinedIcon className="cursor-pointer" />
             English
           </div>
-          <div className="item">
+          <div className="mr-4 flex items-center ">
             <DarkModeOutlinedIcon className="cursor-pointer" />
           </div>
-          <div className="item">
+          <div className="mr-4 flex items-center ">
             <FullscreenExitOutlinedIcon className="cursor-pointer" />
           </div>
-          <div className="item">
+          <div className="relative mr-4 flex items-center ">
             <NotificationsNoneOutlinedIcon className="cursor-pointer" />
+            <div className="text-md absolute right-[-5px] top-[-3px] flex h-[16px] min-h-[16px] w-[16px] min-w-[16px] items-center justify-center rounded-full bg-red-500 font-semibold text-zinc-100">
+              1
+            </div>
           </div>
-          <div className="item">
+          <div className="relative mr-4 flex items-center ">
             <ChatBubbleOutlineOutlinedIcon className="cursor-pointer" />
+            <div className="text-md absolute right-[-5px] top-[-3px] flex h-[16px] min-h-[16px] w-[16px] min-w-[16px] items-center justify-center rounded-full bg-red-500 font-semibold text-zinc-100">
+              2
+            </div>
           </div>
-          <div className="item">
+          <div className="mr-4 flex items-center ">
             <FormatListBulletedOutlinedIcon className="cursor-pointer" />
+          </div>
+          <div className="mr-4 flex items-center ">
+            <img
+              src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg"
+              alt="avatar"
+              className="h-[30px] min-h-[30px] w-[30px] min-w-[30px] rounded-full"
+            />
           </div>
         </div>
       </div>
