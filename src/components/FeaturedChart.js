6eff56aff8a0b0e70589f41d34c8ccdc276e69c2
Craig Barcinas <cbarcinas@live.com>
2023-05-17 13:33:39 -0700

    init

---
+++ src/components/FeaturedChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FeaturedChart = () => {
+  return (
+    <div>
+      <div>
+        <h1>featured chart</h1>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedChart;
---
+++ src/components/GeneralChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GeneralChart = () => {
+  return (
+    <div>
+      <div>
+        <h1>general chart</h1>
+      </div>
+    </div>
+  );
+};
+
+export default GeneralChart;
