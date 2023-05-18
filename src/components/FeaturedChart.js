1dad029c1c1e977bdc90275eaf03e3b49c2b5eef
Craig Barcinas <cbarcinas@live.com>
2023-05-17 20:15:01 -0700

    udpated markup and styles wip

--- src/components/FeaturedChart.js
+++ src/components/FeaturedChart.js
@@ -1,27 +1,61 @@
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const FeaturedChart = () => {
   return (
     <div className=" flex-[2] rounded-xl px-6 py-4 shadow-2xl">
       {/* Top */}
       <div className="flex items-center justify-between">
-        <h1 className="text-xl">Total Revenue</h1>
+        <h1 className="text-xl font-semibold text-zinc-500">Total Revenue</h1>
         <MoreVertIcon fontSize="small" className="cursor-pointer" />
       </div>
       {/* Bottom */}
-      <div className="flex flex-col items-center justify-center gap-2 p-5 text-center">
+      <div className="flex flex-col items-center justify-center gap-2 p-4 text-center">
         <div className="h-28 w-28">
-          <CircularProgressbar value={70} text="70%" strokeWidth={5} />
+          <CircularProgressbar value={70} text="70%" strokeWidth={6} />
         </div>
         <p className="text-lg font-medium text-zinc-600">
           Total Sales made today:
         </p>
-        <p className="text-3xl font-semibold text-zinc-500">$420</p>
+        <p className="text-3xl font-semibold text-zinc-700">$420</p>
         <p className="text-sm text-zinc-600">
-          Lorem ipsum dolor sit, amet consectetur
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, sunt.
         </p>
+        <div className="flex w-full items-center justify-between pt-2 text-center">
+          <div>
+            <div className="text-sm text-zinc-600">Target</div>
+            <div className="mt-2 flex items-center">
+              <KeyboardArrowDownIcon
+                fontSize="small"
+                className="cursor-pointer"
+              />
+              <div>$12.4k</div>
+            </div>
+          </div>
+          <div>
+            <div className="text-sm text-zinc-600">Target</div>
+            <div className="mt-2 flex items-center">
+              <KeyboardArrowDownIcon
+                fontSize="small"
+                className="cursor-pointer"
+              />
+              <div>$12.4k</div>
+            </div>
+          </div>
+          <div>
+            <div className="text-sm text-zinc-600">Target</div>
+            <div className="mt-2 flex items-center">
+              <KeyboardArrowDownIcon
+                fontSize="small"
+                className="cursor-pointer"
+              />
+              <div>$12.4k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
