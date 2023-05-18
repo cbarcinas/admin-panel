5211a3ccb285bbef8fdde9d84b2bdf6c073fa448
Craig Barcinas <cbarcinas@live.com>
2023-05-17 17:14:50 -0700

    imported charts component and structure styles

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,27 @@
 import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
+import Widgets from '../../components/Widgets';
+import FeaturedChart from '../../components/FeaturedChart';
+import GeneralChart from '../../components/GeneralChart';
 
 const Home = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className=" flex-[5]">
+      <div className=" flex-[6]">
         <Navbar />
+        {/* Widgets */}
+        <div className="flex gap-1 p-4">
+          <Widgets type="user" />
+          <Widgets type="order" />
+          <Widgets type="earning" />
+          <Widgets type="balance" />
+        </div>
+        {/* Charts */}
+        <div className="flex gap-5 px-6 py-4">
+          <FeaturedChart />
+          <GeneralChart />
+        </div>
       </div>
     </div>
   );
