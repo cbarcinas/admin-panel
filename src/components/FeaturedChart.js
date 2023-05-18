a6f0c92eb11f188a007946b323e9e72d4e27c472
Craig Barcinas <cbarcinas@live.com>
2023-05-17 17:15:22 -0700

    component wip

--- src/components/FeaturedChart.js
+++ src/components/FeaturedChart.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const FeaturedChart = () => {
   return (
-    <div>
-      <div>
-        <h1>featured chart</h1>
+    <div className=" flex-[2] rounded-xl px-6 py-4 font-Nunito shadow-2xl">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" className="cursor-pointer" />
+      </div>
+      {/* Bottom */}
+      <div className="flex flex-col items-center justify-center gap-2 p-5 text-center">
+        <div className="h-28 w-28">
+          <CircularProgressbar value={70} text="70%" strokeWidth={5} />
+        </div>
+        <p className="text-lg font-medium text-zinc-600">
+          Total Sales made today:
+        </p>
+        <p className="text-3xl font-semibold text-zinc-500">$420</p>
+        <p className="text-sm text-zinc-600">
+          Lorem ipsum dolor sit, amet consectetur
+        </p>
       </div>
     </div>
   );
