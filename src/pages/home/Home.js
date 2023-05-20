620f6aae24be89f2e0a49d2463b2106209d8edf9
Craig Barcinas <cbarcinas@live.com>
2023-05-19 21:37:20 -0700

    imported component and setup styles

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,15 +3,16 @@ import Sidebar from '../../components/Sidebar';
 import Widgets from '../../components/Widgets';
 import FeaturedChart from '../../components/FeaturedChart';
 import Chart from '../../components/Chart';
+import TableDisplay from '../../components/Table';
 
 const Home = () => {
   return (
     <div className="flex font-Nunito">
       <Sidebar />
-      <div className="flex-[6]">
+      <div className="flex-[6] pr-5">
         <Navbar />
         {/* Widgets */}
-        <div className="flex gap-1 p-5">
+        <div className="flex gap-5 p-5">
           <Widgets type="user" />
           <Widgets type="order" />
           <Widgets type="earning" />
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="flex gap-5 px-6 py-4">
           <FeaturedChart />
           <Chart />
+        </div>
+        {/* List */}
+        <div className="m-5 rounded-xl p-3 shadow-2xl">
+          <TableDisplay />
         </div>
       </div>
     </div>
