c96d084213639f5716022d1ca807ba59aadd3165
Craig Barcinas <cbarcinas@live.com>
2023-05-22 18:15:29 -0700

    added comments and cleanup

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,27 +3,28 @@ import Sidebar from '../../components/Sidebar';
 import Widgets from '../../components/Widgets';
 import FeaturedChart from '../../components/FeaturedChart';
 import Chart from '../../components/Chart';
-import TableDisplay from '../../components/Table';
+import TableDisplay from '../../components/TableDisplay';
 
 const Home = () => {
   return (
     <div className="flex font-Nunito">
       <Sidebar />
       <div className="flex-[6] pr-2">
+        {/* Navbar */}
         <Navbar />
-        {/* Widgets */}
+        {/* Widget Section */}
         <div className="flex gap-5 p-5">
           <Widgets type="user" />
           <Widgets type="order" />
           <Widgets type="earning" />
           <Widgets type="balance" />
         </div>
-        {/* Charts */}
+        {/* Charts Section */}
         <div className="flex gap-5 px-6 py-4">
           <FeaturedChart />
           <Chart />
         </div>
-        {/* List */}
+        {/* List Section */}
         <div className="m-5 rounded-xl p-3 shadow-2xl">
           <TableDisplay />
         </div>
