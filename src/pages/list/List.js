ed5204798f30dd91a37294ebe20a4b546679d040
Craig Barcinas <cbarcinas@live.com>
2023-05-23 14:12:08 -0700

    init list page

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import Sidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
+import Datatable from '../../components/Datatable';
 
 const List = () => {
   return (
-    <div>
-      <div>List</div>
+    <div className="flex w-full">
+      <Sidebar />
+      <div className="flex-[6]">
+        <Navbar />
+        <Datatable />
+      </div>
     </div>
   );
 };
