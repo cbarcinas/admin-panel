aaf2e7d631bbc10e15bf94a2ca45e71206bf5eba
Craig Barcinas <cbarcinas@live.com>
2023-05-24 14:04:49 -0700

    added action column

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -2,11 +2,31 @@ import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../datatableSource';
 
 const Datatable = () => {
+  const actionColumn = [
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="flex items-center gap-3">
+            <div className="cursor-pointer rounded-md bg-zinc-200 px-3 py-2 text-zinc-500">
+              View
+            </div>
+            <div className="cursor-pointer rounded-md bg-red-200 px-3 py-2 text-red-500">
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
-    <div className="h-[26rem] p-5">
+    <div className=" p-4">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 5 },
