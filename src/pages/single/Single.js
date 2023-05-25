211bef38a78ecdb70f68eab21ba4617dc543109a
Craig Barcinas <cbarcinas@live.com>
2023-05-24 17:03:26 -0700

    created user info card

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -8,7 +8,50 @@ const Single = () => {
       <Sidebar />
       <div className="flex-[6] pr-2">
         <Navbar />
-        single
+        {/* Top Section */}
+        <div className="flex gap-5 p-5">
+          {/* top left */}
+          <div className="relative min-w-[24rem] flex-1 p-3 shadow-2xl">
+            <div className="absolute right-0 top-0 cursor-pointer rounded-bl-md bg-blue-100 px-2 py-1 text-blue-500">
+              Edit
+            </div>
+            <h1 className="mb-4 text-lg font-semibold text-zinc-500">
+              User Information
+            </h1>
+            <div className="flex">
+              <img
+                src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg"
+                alt="user avatar"
+                className="h-[100px] w-[100px] rounded-full object-cover"
+              />
+              <div className="pl-5">
+                <h3 className="mb-2 text-zinc-700">John Smith</h3>
+                <div className="mb-2 flex flex-col text-sm text-zinc-700">
+                  <span className="itemKey">Email: </span>
+                  <span className="itemValue">jsmith@gmail.com</span>
+                </div>
+                <div className="mb-2 flex flex-col text-sm text-zinc-700">
+                  <span className="itemKey">Phone: </span>
+                  <span className="itemValue">+1 (702)834-1235</span>
+                </div>
+                <div className="mb-2 flex flex-col flex-wrap text-sm text-zinc-700">
+                  <span className="itemKey">Address: </span>
+                  <span className="itemValue">
+                    12345 alphabet dr, Las Vegas, 89102
+                  </span>
+                </div>
+                <div className="mb-2 flex flex-col text-sm text-zinc-700">
+                  <span className="itemKey">Country: </span>
+                  <span className="itemValue">United States</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* top right */}
+          <div className="flex-[2]"></div>
+        </div>
+        {/* Bottom Section */}
+        <div className="bottom"></div>
       </div>
     </div>
   );
