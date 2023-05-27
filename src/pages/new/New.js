e944ffb23618d246b907010e93051ec62960f4e7
Craig Barcinas <cbarcinas@live.com>
2023-05-26 23:07:51 -0700

    udpated styles: mvp

--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -8,13 +8,13 @@ const New = ({ title, inputs }) => {
       <Sidebar />
       <div className="flex-[6]">
         <Navbar />
-        {/* Top */}
-        <div className="m-5 flex p-3 shadow-2xl">
+        {/* Top Section */}
+        <div className="m-5 flex w-max p-3 shadow-2xl">
           <h1 className="text-xl font-semibold text-zinc-500">{title}</h1>
         </div>
-        {/* Bottom */}
-        <div className="m-5 flex min-w-[41rem] max-w-[41rem] gap-5 p-3 shadow-2xl">
-          {/* Bottom left */}
+        {/* Bottom Section */}
+        <div className="m-5 flex min-w-[30rem] max-w-[30rem] flex-col gap-5 p-3 shadow-2xl">
+          {/* Bottom left (display image) */}
           <div className="flex flex-1 justify-center">
             <img
               className="h-[100px] w-[100px] rounded-full object-cover"
@@ -22,30 +22,32 @@ const New = ({ title, inputs }) => {
               alt="img"
             />
           </div>
-          {/* Bottom right (Form) */}
-          <div className="flex-[2]">
-            <form className="flex flex-wrap justify-around gap-3 pr-3">
-              {/* user img upload */}
-              <div className="w-[40%]">
-                <label htmlFor="file" className="cursor-pointer">
-                  Image: <DriveFolderUploadOutlinedIcon />
-                </label>
-                <input type="file" id="file" style={{ display: 'none' }} />
-              </div>
-              {/* map over form source for user inputs */}
-              {inputs.map((input) => (
-                <div className="w-[40%]" key={input.id}>
-                  <label className="flex items-center gap-5">
-                    {input.label}
+          {/* Bottom right (form container) */}
+          <div className="flex-[2] p-4">
+            <form className="flex flex-col items-center justify-center">
+              <div className="grid grid-cols-2">
+                {/* Map over form source for user/product inputs */}
+                {inputs.map((input) => (
+                  <div className="mb-5" key={input.id}>
+                    <label className="font-semibold text-zinc-600">
+                      {input.label}
+                    </label>
+                    <input
+                      className="border-b-[1px] border-solid border-zinc-500"
+                      type={input.type}
+                      placeholder={input.placeholder}
+                    />
+                  </div>
+                ))}
+                {/* user img upload */}
+                <div>
+                  <label htmlFor="file" className="cursor-pointer">
+                    Image: <DriveFolderUploadOutlinedIcon />
                   </label>
-                  <input
-                    className="border-b-[1px] border-solid border-zinc-500"
-                    type={input.type}
-                    placeholder={input.placeholder}
-                  />
+                  <input type="file" id="file" style={{ display: 'none' }} />
                 </div>
-              ))}
-              <button className="w-[150px] rounded-md bg-blue-500 p-2 text-lg font-semibold tracking-wide text-white">
+              </div>
+              <button className="w-[150px] rounded-md bg-blue-500 py-2 text-lg font-semibold tracking-wide text-white transition-all duration-150 ease-in-out hover:scale-[104%]">
                 Submit
               </button>
             </form>
